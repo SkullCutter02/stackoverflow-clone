80cbc889c44c6b039a0fdd620c58c1c399577f92
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-22 12:40:25 +0800

    Postgres database connected, sequelize initial workflow complete

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { sequelize } = require("./models");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started on ${PORT}`);
+  await sequelize.authenticate();
+  console.log("Database Connected!");
 });
