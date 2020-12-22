0c64fd35fcd4b580ff7f06989f4f773a94c35bd8
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-22 12:48:20 +0800

    Users model and migration complete

---
+++ backend/migrations/20201222044253-create-user.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      verified: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
