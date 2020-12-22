4279cb6cb79fda2e92226061b3526ae0b4156085
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-22 12:50:02 +0800

    cors and helmet modules adde

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const { sequelize } = require("./models");
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 const PORT = process.env.PORT || 5000;
 
