69c6d0a749eca5965fdddb0455f99dc39b360217
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-22 20:11:39 +0800

    /login route added

--- backend/app.js
+++ backend/app.js
@@ -56,6 +56,42 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, email, password, rememberMe } = req.body;
+  const searchByUsername = !!username;
+
+  const user = searchByUsername
+    ? await User.findOne({
+        where: { username: username },
+      })
+    : await User.findOne({
+        where: { email: email },
+      });
+
+  if (user) {
+    const isValidPass = await comparePassword(password, user.hash);
+
+    if (isValidPass) {
+      jwt.sign({ id: user.id }, "secretkey", (err, token) => {
+        if (err) {
+          return res.status(500).json({ err });
+        } else {
+          if (!rememberMe) {
+            res.cookie("token", token);
+          } else {
+            res.cookie("token", token, { maxAge: 31536000000 });
+          }
+          return res.json({ token });
+        }
+      });
+    } else {
+      return res.status(500).json({ msg: "Invalid Credentials" });
+    }
+  } else {
+    return res.status(500).json({ msg: "Invalid Credentials" });
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
