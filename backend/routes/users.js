378bdbd6711935d21fdd36e475a2c635dc336529
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-23 21:10:39 +0800

    Added separate route to get the comments and posts of a user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { User, Post, Community } = require("../models");
+const { User, Post, Community, Comment } = require("../models");
 
 const router = express.Router();
 
@@ -8,15 +8,59 @@ router.get("/:uuid", async (req, res) => {
     const { uuid } = req.params;
     const user = await User.findOne({
       where: { uuid: uuid },
+    });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ err });
+  }
+});
+
+router.get("/:uuid/posts", async (req, res) => {
+  try {
+    const { uuid } = req.params;
+    const user = await User.findOne({
+      where: { uuid: uuid },
       include: [
         {
           model: Post,
           as: "posts",
-          include: {
-            model: Community,
-            as: "communities",
-            through: { attributes: [] },
-          },
+          include: [
+            {
+              model: Community,
+              as: "communities",
+              through: { attributes: [] },
+            },
+            {
+              model: Comment,
+              as: "comments",
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ err });
+  }
+});
+
+router.get("/:uuid/comments", async (req, res) => {
+  try {
+    const { uuid } = req.params;
+    const user = await User.findOne({
+      where: { uuid: uuid },
+      include: [
+        {
+          model: Comment,
+          as: "comments",
+          include: [
+            {
+              model: Post,
+              as: "post",
+            },
+          ],
         },
       ],
     });
