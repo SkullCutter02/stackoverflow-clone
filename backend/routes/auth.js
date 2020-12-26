cf7a6c26de807a0f8a11c757e44ced955a67825e
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-26 16:33:56 +0800

    Added validation to /signup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,55 +5,84 @@ const { User, Email } = require("../models");
 const { hashPassword, comparePassword } = require("../hash");
 const transporter = require("../transporter");
 const { signUpLimit, logInLimit, forgetPasswordLimit } = require("../limiters");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-router.post("/signup", signUpLimit, async (req, res) => {
-  try {
-    const { username, email, password, rememberMe } = req.body;
-
-    if (username && email && password && rememberMe !== null) {
-      const checkUsername = await User.findOne({
-        where: { username: username },
-      });
-      const checkEmail = await User.findOne({
-        where: { email: email },
-      });
-
-      if (checkUsername) {
-        return res.status(500).json({ msg: "Username already taken" });
-      } else if (checkEmail) {
-        return res.status(500).json({ msg: "Email already exists" });
-      } else {
-        const hash = await hashPassword(password);
-        const user = await User.create({ username, email, hash });
-
-        if (user) {
-          const uuid = user.uuid;
-          jwt.sign({ uuid }, "secretkey", (err, token) => {
-            if (err) {
-              return res.status(500).json({ err });
-            } else {
-              if (!rememberMe) {
-                res.cookie("token", token);
-              } else {
-                res.cookie("token", token, { maxAge: 31536000000 });
-              }
-              return res.json({ token });
-            }
-          });
-        } else {
-          return res.status(500).json({ msg: "Signup failed" });
-        }
-      }
-    } else {
-      return res.status(500).json({ msg: "Please enter the correct fields" });
+router.post(
+  "/signup",
+  signUpLimit,
+  check("username")
+    .isLength({ max: 20 })
+    .withMessage("Username must not be longer than 20 characters")
+    .matches(/^[/a-zA-Z0-9_-]*$/)
+    .withMessage(
+      "Username can only contain letters, numbers, dash and underscore"
+    ),
+  check("email").isEmail().withMessage("Please enter a valid email"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be more than 8 characters long")
+    .isLength({ max: 20 })
+    .withMessage("Password cannot be longer than 20 characters")
+    .matches(/^.*(?=.{8,20})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/)
+    .withMessage(
+      "Password must contain at least one uppercase, one lowercase, and one special character"
+    ),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(403)
+        .json({ errors: errors.array({ onlyFirstError: true }) });
     }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
+
+    try {
+      const { username, email, password, rememberMe } = req.body;
+
+      if (username && email && password && rememberMe !== null) {
+        const checkUsername = await User.findOne({
+          where: { username: username },
+        });
+        const checkEmail = await User.findOne({
+          where: { email: email },
+        });
+
+        if (checkUsername) {
+          return res.status(500).json({ msg: "Username already taken" });
+        } else if (checkEmail) {
+          return res.status(500).json({ msg: "Email already exists" });
+        } else {
+          const hash = await hashPassword(password);
+          const user = await User.create({ username, email, hash });
+
+          if (user) {
+            const uuid = user.uuid;
+            jwt.sign({ uuid }, "secretkey", (err, token) => {
+              if (err) {
+                return res.status(500).json({ err });
+              } else {
+                if (!rememberMe) {
+                  res.cookie("token", token);
+                } else {
+                  res.cookie("token", token, { maxAge: 31536000000 });
+                }
+                return res.json({ token });
+              }
+            });
+          } else {
+            return res.status(500).json({ msg: "Signup failed" });
+          }
+        }
+      } else {
+        return res.status(500).json({ msg: "Please enter the correct fields" });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
   }
-});
+);
 
 router.post("/login", logInLimit, async (req, res) => {
   try {
